d70117e5157bb1ca575a95c0f0d39403fb51c562
Marouane Mounib <mamounib@e2r3p5.1337.ma>
2023-03-11 14:33:18 +0100

    add header main.h to main adn add file main.h

--- program/main.h
+++ program/main.h
@@ -1,24 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/11 14:05:02 by mamounib          #+#    #+#             */
-/*   Updated: 2023/03/11 14:31:42 by mamounib         ###   ########.fr       */
+/*   Created: 2023/03/11 14:28:25 by mamounib          #+#    #+#             */
+/*   Updated: 2023/03/11 14:30:29 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "main.h"
-
-int main(int argc, char **argv, char **environ)
-{
-	char **env = environ;
-
-	while (*env)
-	{
-		puts(*env++);
-	}
-	return 0;
-}
+#ifndef MAIN_H
+# define MAIN_H
+# include <stdlib.h>
+#endif 
