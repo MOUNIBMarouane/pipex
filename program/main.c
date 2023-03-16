75fcee01a89024397bb94a5fe0c7b4dbc51759f6
Marouane Mounib <mamounib@e2r3p3.1337.ma>
2023-03-16 17:39:37 +0100

    fix executing

--- program/main.c
+++ program/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: killwa <killwa@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 14:05:02 by mamounib          #+#    #+#             */
-/*   Updated: 2023/03/16 12:16:30 by killwa           ###   ########.fr       */
+/*   Updated: 2023/03/16 17:07:13 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 int main(int argc, char **argv, char **environ)
 {
-	char *executing;
-	char *flags;
-	char **cmd_flag;
-	int i = 1;
+	char	*executing;
+	char	**cmd_flag;
+	int	i;
 	int check;
+
+	i = 1;
 	printf("%d\n", argc);
-	while (i < argc )
+	while (i < argc)
 	{
 		executing = NULL;
 		check = ft_checkargs(argv[i], environ);
@@ -30,8 +31,7 @@ int main(int argc, char **argv, char **environ)
 			// puts("split the arg and get the first then exute with flag");
 			cmd_flag= ft_split(argv[i], ' ');
 			executing = ft_executing(cmd_flag[0], environ);
-			flags= cmd_flag[1];
-			printf("cmd :%s flag: %s\n", executing, flags);
+			printf("cmd :%s", executing);
 		}
 		else if (check == 2)
 		{
@@ -39,7 +39,9 @@ int main(int argc, char **argv, char **environ)
 			printf("cmd : %s \n", executing);
 		} 
 		i++;
+		fork();
+		execve(executing,ft_split("ls -a a.out", ' '),NULL);
 	}
-	
+	// execve("/bin/ls",ft_split("ls -a a.out", ' '),NULL);
 	return 0;
 }
